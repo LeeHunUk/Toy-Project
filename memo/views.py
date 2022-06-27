55642bd14cf1d8babd0a8f952650d261f2e55556
LeeHunUk <gnsdnr1022@gmail.com>
2022-06-27 11:25:44 +0900

    빈 오류 수정

--- memo/views.py
+++ memo/views.py
@@ -15,8 +15,4 @@ def index(request):
     view_paginator = Paginator(view, 6)
     view = view_paginator.get_page(1)
 
-    best = (
-        Memos.objects.order_by("-view")[0],
-        Memos.objects.order_by("-like")[0])
-
     return render(request, "main.html", {"labels": labels, "like": like, "view": view})
